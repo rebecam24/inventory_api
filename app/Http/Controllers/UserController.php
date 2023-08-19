159e9b7df3b6e983ed2186115619745941dd58a7
Rebeca <120200340+rebecam24@users.noreply.github.com>
2023-08-18 21:15:08 -0500

    add: :strawberry: CRUD users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Traits\ApiResponse;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -15,7 +16,11 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::all();
+
+        return $this->success([
+            'users' => $users,
+        ],'Users list');
     }
 
     /**
@@ -26,9 +31,29 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
 
+
+        $data = $request->validate([
+            'role_id'       => ['required','integer','exists:roles,id'],
+            'email'         => ['required','string','email','unique:users,email'],
+            'password'      => ['required','string','min:8'],
+            'name'          => ['required','string','min:3','max:45'],
+            'lastname'      => ['required','string','min:3','max:45'],
+            'birthday'      => ['required','date'],
+            'phone'         => ['required','string','min:7','max:15'],
+            'id_number'     => ['required','string','min:3','max:45'],
+            'address'       => ['required','string','min:10','max:100'],
+            'gender'        => ['required','string','max:45'],
+            'url_image'     => ['nullable','url'],
+            'work_position' => ['nullable','string','max:45'],
+        ]);
+
+        $data['password'] = bcrypt($data['password']);
+        $user = User::create($data);
+        return $this->success([
+            'user'  => $user,
+        ],'Registered User');
+    }
     /**
      * Display the specified resource.
      *
@@ -37,7 +62,11 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        return $this->success([
+            'user' => $user,
+        ],'User details');
     }
 
     /**
@@ -49,7 +78,33 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+
+        $data = $request->validate([
+            'role_id'       => ['nullable','integer','exists:roles,id'],
+            'email'         => ['nullable','string','email','unique:users,email'],
+            'password'      => ['nullable','string','min:8'],
+            'name'          => ['nullable','string','min:3','max:45'],
+            'lastname'      => ['nullable','string','min:3','max:45'],
+            'birthday'      => ['nullable','date'],
+            'phone'         => ['nullable','string','min:7','max:15'],
+            'id_number'     => ['nullable','string','min:3','max:45'],
+            'address'       => ['nullable','string','min:10','max:100'],
+            'gender'        => ['nullable','string','max:45'],
+            'url_image'     => ['nullable','url'],
+            'work_position' => ['nullable','string','max:45']
+        ]);
+        // return response()->json($data);
+
+        if(isset($data['password'])){
+            $data['password'] = bcrypt($data['password']);
+        }
+
+        $user = User::findOrFail($id);
+        $user->update($data);
+
+        return $this->success([
+            'user'  => $user,
+        ],'Updated User');
     }
 
     /**
@@ -60,6 +115,9 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::find($id);
+        $user->delete();
+
+        return $this->success([],'User Deleted ');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -50,11 +50,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function invoices(){
-        return $this->hasMany(Invoice::class);
-    }
+    // public function invoices(){
+    //     return $this->hasMany(Invoice::class);
+    // }
 
-    public function role(){
-        return $this->belongsTo(Role::class);
-    }
+    // public function role(){
+    //     return $this->belongsTo(Role::class);
+    // }
 }
