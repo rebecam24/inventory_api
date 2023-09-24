c613a16fffacb661c0729211d1dc52bc567f9343
Rahisbel <rh@plusteam.tech>
2023-09-24 13:09:51 -0400

    feat: :elephant: CRUD of categories

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,4 +20,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('users', UserController::class);
+Route::post('/login',[LoginController::class,'login']);
+
+Route::middleware('auth:sanctum')->group( function(){
+
+    Route::post('/logout',[LoginController::class,'logout']);
+
+    // Users
+    Route::apiResource('users',UserController::class)->middleware('admin','mod');
+
+    // Categories
+    Route::apiResource('categories',CategoryController::class)->middleware('admin','mod');
+});
