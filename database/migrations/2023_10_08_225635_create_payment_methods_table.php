cadfb63304a24a0c36494d00acd11f06148ca9cc
Rebeca <120200340+rebecam24@users.noreply.github.com>
2023-10-08 18:30:43 -0500

    update: :strawberry: migration

--- database/migrations/2023_10_08_225635_create_payment_methods_table.php
+++ database/migrations/2023_10_08_225635_create_payment_methods_table.php
@@ -15,7 +15,6 @@ class CreatePaymentMethodsTable extends Migration
     {
         Schema::create('payment_methods', function (Blueprint $table) {
             $table->id();
-            $table->id();
             $table->string('type',45);
             $table->timestamps();
         });
